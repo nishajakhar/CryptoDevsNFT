15dbf85cfd9cf76d27c74efbc2f12c4172ab0185
nishajakhar <ruhaniroy26396@gmail.com>
2022-08-15 19:51:02 +0530

    feat(): Crypto Dev NFT contract completed.

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,18 +1,23 @@
 // Import ethers from Hardhat package
 const { ethers } = require("hardhat");
 
+// Replace address of your whitelist contract
+const WHITELIST_CONTRACT_ADDRESS = "0xBd28d3D193b10443C6456F96ec92f24450BecA8a";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
 async function main() {
-  /*
-A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-so nftContract here is a factory for instances of our GameItem contract.
-*/
-  const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-  // here we deploy the contract  
-  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // Whitelist Contract
+  // const whitelistContract = await ethers.getContractFactory("Whitelist");
+  // const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
 
-  // print the address of the deployed contract
-  console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
+  // After you deploy Whitelist, We need to pass its address to CryptoDevs
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(METADATA_URL, WHITELIST_CONTRACT_ADDRESS);
+  console.log("Whitelist Contract Address:", deployedCryptoDevsContract.address);
+
 }
 
 // Call the main function and catch if there is any error
