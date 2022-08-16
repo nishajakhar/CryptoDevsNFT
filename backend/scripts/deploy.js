ceb0a3e281fa8ff0b39e1ece85af32e0cd5a51fb
nishajakhar <ruhaniroy26396@gmail.com>
2022-08-16 22:45:43 +0530

    feat(): Crypto Dev ICO contract created and deploy script updated

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -3,6 +3,8 @@ const { ethers } = require("hardhat");
 
 // Replace address of your whitelist contract
 const WHITELIST_CONTRACT_ADDRESS = "0xBd28d3D193b10443C6456F96ec92f24450BecA8a";
+const NFT_CONTRACT_ADDRESS = "0xb0892FCCC34bb99a4e5EEdDc2E2AA51797843D65";
+
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
@@ -14,9 +16,15 @@ async function main() {
   // console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
 
   // After you deploy Whitelist, We need to pass its address to CryptoDevs
-  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
-  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(METADATA_URL, WHITELIST_CONTRACT_ADDRESS);
-  console.log("Whitelist Contract Address:", deployedCryptoDevsContract.address);
+  // const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+  // const deployedCryptoDevsContract = await cryptoDevsContract.deploy(METADATA_URL, WHITELIST_CONTRACT_ADDRESS);
+  // console.log("Whitelist Contract Address:", deployedCryptoDevsContract.address);
+
+  // After you deploy Crypto Dev, We need to pass its address to CryptoDevToken
+  const cryptoDevTokenContract = await ethers.getContractFactory("CryptoDevToken");
+  const deployedCryptoDevTokenContract = await cryptoDevTokenContract.deploy(NFT_CONTRACT_ADDRESS);
+  console.log("Crypto Dev ICO Contract Address:", deployedCryptoDevTokenContract.address);
+
 
 }
 
